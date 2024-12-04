b7a71dfd14893d88fbc4d3dc0f67d28f827fb3d1
Sebastian Stark <sstark@mailbox.org>
2024-12-04 12:54:43 +0100

    much better

--- 1/1_2.rb
+++ 1/1_2.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
 
 left, right = ARGF.map(&:split).transpose
-freqs = Hash[right.group_by{|x|x}.map{|k, v| [k.to_i, v.size]}]
-puts left.map {|e| freqs.fetch(e.to_i, 0) * e.to_i}.sum
+puts left.map {|e| right.tally.fetch(e, "0").to_i * e.to_i}.sum
