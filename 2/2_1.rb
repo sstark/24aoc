b4696cd26d279ad0d4e6ff3536fb5910a3a8713b
Sebastian Stark <sstark@mailbox.org>
2024-12-04 12:25:59 +0100

    Comment

--- 2/2_1.rb
+++ 2/2_1.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
 m = ARGF.map { |x| x.split.map(&:to_i) }
+# Array#each_cons is like Python's itertools.pairwise, so the following
+# is something like an indexed map of the first derivative
 inc = m.map { |x| x.each_cons(2).map {|y,z|y-z} }
 class Array
   def valid?
